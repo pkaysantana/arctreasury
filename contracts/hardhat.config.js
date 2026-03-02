28cf343ea11d2bbf4ab977796ce401b92b704c52
Don Aborah <aborahdon@gmail.com>
2026-03-02 01:54:55 +0000

    feat: initialise Hardhat project with configuration and dependencies for smart contract development

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -5,6 +5,7 @@ module.exports = {
     solidity: {
         version: "0.8.24",
         settings: {
+            evmVersion: "cancun",
             optimizer: {
                 enabled: true,
                 runs: 200,
